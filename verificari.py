6b08f020ef9db95b5265068ba60574c6eb1e7d68
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-06-18 14:12:59 +0300

    Update verificari.py

--- verificari.py
+++ verificari.py
@@ -1,5 +1,6 @@
 import openpyxl
 from openpyxl import load_workbook
+import time
 
 def check_excel_template(excel_file):
     """
@@ -11,12 +12,17 @@ def check_excel_template(excel_file):
     Returns:
     bool: True dacă foaia '1A-Bilant' este prezentă, altfel False
     """
+    start_time = time.time()
     try:
         workbook = load_workbook(excel_file, data_only=True)
         sheet_names = workbook.sheetnames
         if "1A-Bilant" in sheet_names:
+            st.write(f"Sheet check completed in {time.time() - start_time} seconds")
             return True
         else:
+            st.write(f"Sheet check completed in {time.time() - start_time} seconds")
             return False
     except Exception as e:
+        st.error(f"Eroare la încărcarea fișierului Excel: {e}")
         return False
+
