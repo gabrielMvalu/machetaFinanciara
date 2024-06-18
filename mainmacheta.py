a328138217633241f05d44c7abc94ac2b383f64e
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-06-18 14:01:03 +0300

    Update mainmacheta.py

--- mainmacheta.py
+++ mainmacheta.py
@@ -11,6 +11,6 @@ excel_file = st.file_uploader("Încarcă Macheta Financiară (Excel)", type=["xl
 
 # Afișează mesaj de confirmare a încărcării fișierelor
 if pdf_file is not None:
-    st.success("Bilanțul Contabil a fost încărcat cu succes.")
+    st.tost("Bilanțul Contabil a fost încărcat cu succes.")
 if excel_file is not None:
-    st.success("Macheta Financiară a fost încărcată cu succes.")
+    st.tost("Macheta Financiară a fost încărcată cu succes.")
