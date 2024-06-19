6f000bc066fed8b8c1ed7b59ef66a4a774e200a2
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-06-19 13:46:10 +0300

    Update mainmacheta.py

--- mainmacheta.py
+++ mainmacheta.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from verificari import extract_data_from_pdf
+from verificari import find_page_with_phrase
 
 # Titlul aplicației
 st.title("Automatizare Machetă Financiară")
